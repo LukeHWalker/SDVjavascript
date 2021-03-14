63ef44615c58877efd1bc54ca9d03ab1c09e1335
LukeHWalker <79427059+LukeHWalker@users.noreply.github.com>
2021-03-15 12:50:57 +1300

    added to week4.js

---
+++ src/week4.js
@@ -0,0 +1,23 @@
+//Values and variables
+// declared variable country a string value of "New Zealand"
+let country = "New Zealand"
+let city = "nelson"
+let postalcode = 7010
+
+console.log(country);
+console.log(city);
+console.log(postalcode);
+
+//let allows a variable to be undefined for later use
+let isIsland = true
+let language 
+
+console.log(isIsland)
+console.log(language)
+
+//typeof prints out what type of variable is being used
+console.log(typeof isIsland)
+
+//Reserved keywords cannot be used as a variable
+//let typeof
+
